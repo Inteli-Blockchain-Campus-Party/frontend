ed0eeadfe29ca693a1c1937a75412841b2618ce1
feitozamarcelo <feitoza.marcelo7@gmail.com>
2022-11-13 18:45:57 -0300

    deploy try

---
+++ build/server/chunks/482.js
@@ -0,0 +1,46 @@
+"use strict";
+exports.id = 482;
+exports.ids = [482];
+exports.modules = {
+
+/***/ 1482:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "N": () => (/* binding */ PublicIcon)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+
+const PublicIcon = (props)=>{
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("svg", {
+        xmlns: "http://www.w3.org/2000/svg",
+        width: props.width ? props.width : "61",
+        height: props.height ? props.height : "61",
+        fill: "none",
+        viewBox: "0 0 61 61",
+        ...props,
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("path", {
+                stroke: "#fff",
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeWidth: "5.083",
+                d: "M30.5 55.917c14.037 0 25.417-11.38 25.417-25.417S44.537 5.083 30.5 5.083 5.083 16.463 5.083 30.5 16.463 55.917 30.5 55.917zM5.083 30.5h50.834"
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("path", {
+                stroke: "#fff",
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeWidth: "5.083",
+                d: "M30.5 5.083A38.888 38.888 0 0140.667 30.5 38.888 38.888 0 0130.5 55.917 38.888 38.888 0 0120.333 30.5 38.887 38.887 0 0130.5 5.083v0z"
+            })
+        ]
+    });
+};
+
+
+/***/ })
+
+};
+;
