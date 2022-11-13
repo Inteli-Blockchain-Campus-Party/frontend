7e70c7817aa9b15e7db4743ac26d583f565d8b16
TechioRafael <110608373+RafaelTechio@users.noreply.github.com>
2022-11-13 05:15:54 -0300

    FEAT: initializing first page and public assets
    Co-authored-by: Marcelo G Feitoza <marcelofeitoza@users.noreply.github.com>

--- src/Screens/Index/IndexScreen.js
+++ src/Screens/Index/IndexScreen.js
@@ -1,18 +1,27 @@
 import axios from "axios";
 import React, { useState } from "react";
 
+import styles from './IndexScreen.module.css'
+
+import Logo from '../../logo.svg'
+
 const IndexScreen = () => {
     const [test, setTest] = useState({});
 
     axios.get("https://inteli-blockchain-campus-party.herokuapp.com/").then((value) => {
-        setTest({"resposta da api": value.data});
+        setTest({ "resposta da api": value.data });
     });
 
-    return <main>
+    return <main className={styles.Body}>
+        <img src={Logo} alt="Health Vault logo" />
         <div>
-            {JSON.stringify(test)}
+            <span>Everything you doctor needs to know about you. Where and when you most need it.</span>
+            <div>
+                <button className={styles.FirstButton}>I want to add my records</button>
+                <button className={styles.SecondButton}>I want to see someone's record</button>
+            </div>
         </div>
-        <h1>Index</h1>
+
     </main>
 }
 
--- src/Screens/NotFoundScreen/NotFoundScreen.js
+++ src/Screens/NotFoundScreen/NotFoundScreen.js
@@ -3,9 +3,16 @@ import React from "react";
 import styles from "./NotFoundScreen.module.css";
 
 const IndexScreen = () => {
-    return <main>
-        <h1 className={styles.teste}>Page Not Found</h1>
-    </main>
+    return (
+        <main className={styles.content}>
+            <h1 className={styles.mainText}>
+                404
+            </h1>
+            <p className={styles.subText}>
+                Page not found
+            </p>
+        </main>
+    )
 }
 
 export default IndexScreen
