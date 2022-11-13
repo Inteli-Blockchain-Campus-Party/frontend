3ca0fbc603673c379728e8b9ad8dddcbaa805e4f
feitozamarcelo <feitoza.marcelo7@gmail.com>
2022-11-13 08:47:55 -0300

    pages

--- pages/createRecord.js
+++ pages/createRecord.js
@@ -10,6 +10,8 @@ const Register = () => {
       treatments: ["Benadryl", "Zyrtec"],
       date: "",
       type: "allergy",
+      description: "I have a peanut allergy",
+      place: "Home",
     },
     {
       type: "allergy",
@@ -17,9 +19,12 @@ const Register = () => {
       symptoms: ["Itchy throat", "Itchy eyes"],
       treatments: ["Benadryl", "Zyrtec"],
       date: "",
+      description: "I have a shrimp allergy",
+      place: "",
     },
     {
       title: "Diabetes",
+      place: "",
       symptoms: [
         "Frequent urination",
         "Increased thirst",
@@ -33,16 +38,20 @@ const Register = () => {
       treatments: ["Insulin", "Metformin", "Diet", "Exercise"],
       date: "",
       type: "condition",
+      description: "",
     },
     {
       title: "Asthma",
+      place: "",
       symptoms: ["Coughing", "Wheezing", "Dizziness", "Loss of consciousness"],
       treatments: ["Albuterol", "Ipratropium", "Prednisone"],
       date: "",
       type: "condition",
+      description: "I last had an asthma attack in 2019",
     },
     {
       title: "Flu",
+      place: "",
       symptoms: [
         "Fever",
         "Cough",
@@ -53,20 +62,25 @@ const Register = () => {
       treatments: ["Rest", "Fluids", "Tylenol"],
       date: "",
       type: "condition",
+      description: "",
     },
     {
       type: "surgery",
+      place: "Hospital Sirio Libanes",
       title: "Appendectomy",
       date: "10/10/2020",
       treatments: [],
       symptoms: [],
+      description: "",
     },
     {
       type: "surgery",
       title: "Tonsillectomy",
       date: "10/10/2020",
+      place: "Hospital Sirio Libanes",
       treatments: [],
       symptoms: [],
+      description: "I had my tonsils removed",
     },
   ]);
 
@@ -172,6 +186,46 @@ const Register = () => {
                           </p>
                         ))}
                       </div>
+                    </div>
+                  ) : null}
+                </div>
+
+                <div>
+                  {highlighted.treatments.length > 0 ? (
+                    <div>
+                      <p className="text-xl">Treatments</p>
+                      <div className="flex flex-row flex-wrap">
+                        {highlighted.treatments.map((item) => (
+                          <p
+                            className="text-white bg-[#1460D294] rounded-lg px-2 py-1 m-1"
+                            key={item}
+                          >
+                            {item}
+                          </p>
+                        ))}
+                      </div>
+                    </div>
+                  ) : null}
+                </div>
+
+                <div>
+                  {highlighted.date ? (
+                    <div className="flex flex-row justify-between">
+                      <div>
+                        <p className="text-xl">Date</p>
+                        <p className="text-white bg-[#1460D294] rounded-lg px-2 py-1 m-1">
+                          {highlighted.date}
+                        </p>
+                      </div>
+
+                      {highlighted.place ? (
+                        <div>
+                          <p className="text-xl">Place</p>
+                          <p className="text-white bg-[#1460D294] rounded-lg px-2 py-1 m-1">
+                            {highlighted.place}
+                          </p>
+                        </div>
+                      ) : null}
                     </div>
                   ) : null}
                 </div>
