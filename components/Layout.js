e1a3860f48e65e24a0bdec5bbe7d8dbed5eeee95
feitozamarcelo <feitoza.marcelo7@gmail.com>
2022-11-13 17:34:21 -0300

    build

--- components/Layout.js
+++ components/Layout.js
@@ -4,6 +4,8 @@ export const Layout = ({ children, title, props }) => (
   <div>
     <Head>
       <title>{title ? title + " - HealthVault" : "HealthVault"}</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="icon" href="/favicon.ico" />
     </Head>
 
     {children}
